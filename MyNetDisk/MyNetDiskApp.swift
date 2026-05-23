46a0d1ab84bd53993968e7588036e8a252e3e740
chenshiyu <545711@qq.com>
2026-05-23 16:19:48 +0800

    Initial Commit

---
+++ MyNetDisk/MyNetDiskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNetDiskApp.swift
+//  MyNetDisk
+//
+//  Created by chenshiyu on 2026/5/23.
+//
+
+import SwiftUI
+
+@main
+struct MyNetDiskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
